823f4782339271ebab1c15bcc4fee9be2a2921bf
Peter Wagner <pwagner@pebble.com>
2015-12-01 20:53:48 -0500

    Minimal DynamoDB IAM for workers/agents
    Updating demo.py, we're back in business!

--- src/agent.py
+++ src/agent.py
@@ -4,16 +4,18 @@ import os
 import boto.ec2.elb
 from systemd.manager import Manager
 from main import get_instance_id, setup_logging
-from flotilla.agent import FlotillaAgent, FlotillaAgentDynamo, SystemdUnits
+from flotilla.agent import FlotillaAgent, FlotillaAgentDynamo, LoadBalancer, \
+    SystemdUnits
 from flotilla.db import DynamoDbTables, DynamoDbLocks
 from flotilla.thread import RepeatingFunc
 
 
-def get_elb():
+def get_elb(id):
     elb_name = os.environ.get('FLOTILLA_LB')
     if elb_name:
         elb_region = os.environ.get('FLOTILLA_LB_REGION', 'us-west-2')
-        return boto.ec2.elb.connect_to_region(elb_region)
+        elb = boto.ec2.elb.connect_to_region(elb_region)
+        return LoadBalancer(id, elb_name, elb)
     else:
         return None
 
@@ -30,7 +32,7 @@ if __name__ == '__main__':
     systemd = SystemdUnits(manager)
 
     # AWS services:
-    elb = get_elb()
+    lb = get_elb(instance_id)
     dynamo = boto.dynamodb2.connect_to_region('us-east-1')
 
     # DynamoDB:
@@ -42,7 +44,7 @@ if __name__ == '__main__':
     locks = DynamoDbLocks(instance_id, tables.locks)
 
     # Assemble into agent:
-    agent = FlotillaAgent(service, db, locks, systemd, elb)
+    agent = FlotillaAgent(service, db, locks, systemd, lb)
 
     # Start loops:
     funcs = [
